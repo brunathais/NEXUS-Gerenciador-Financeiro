ca770c544e47cd32df5e6c7484e6c90be00ccb88
EstudanteXAnonimo <ricardo_dotto@estudante.sesisenai.org.br>
2025-10-07 20:27:50 -0300

    Adição do arquivo .tsx do gráfico. Estou trabalhando em cima de sua implementação, pois ainda não é possível visualizar o gráfico Donut e Bar.

---
+++ frontend/src/components/Graficos.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './graficos.css';
+
+export function Graficos() {
+
+    return (
+        <div className="grafico-container">
+            <h2>Controle Financeiro</h2>
+        </div>
+    );
+
+}
---
+++ frontend/src/components/OrcamentoForm.tsx
@@ -0,0 +1,6 @@
+// filepath: ./components/OrcamentoForm.tsx
+const OrcamentoForm = () => {
+  return <div className="orcamentoForm">OrcamentoForm Component</div>;
+};
+
+export default OrcamentoForm;
